c3861b6837f498189731b74fbf27979f4503502d
Rohit Godbole <rgodbole@ucsd.edu>
2017-05-03 12:06:57 -0700

    starts Choose Photo activity

--- app/src/main/java/com/example/cs110sau/dejaphoto/MainActivity.java
+++ app/src/main/java/com/example/cs110sau/dejaphoto/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.cs110sau.dejaphoto;
 
 import android.app.WallpaperManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -21,11 +26,38 @@ public class MainActivity extends AppCompatActivity {
     ImageView default_img;
 
     public void changeWallpaper (View view) {
+
+        System.out.println("TEST");
+
+        Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(i, 1);
+
         WallpaperManager w = WallpaperManager.getInstance (getApplicationContext());
         try {
             w.setResource( + R.drawable.wallpaper);
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
+        /*
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.default_img);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        */
         }
     }
 
