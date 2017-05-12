af3904d79f73bbb2003984670d93bd73b422c712
Rohit Godbole <rgodbole@ucsd.edu>
2017-05-11 17:10:45 -0700

    app successfully gets Uris

--- app/src/main/java/com/example/cs110sau/dejaphoto/MainActivity.java
+++ app/src/main/java/com/example/cs110sau/dejaphoto/MainActivity.java
@@ -1,15 +1,23 @@
 package com.example.cs110sau.dejaphoto;
 
+import android.Manifest;
+import android.app.Activity;
 import android.app.WallpaperManager;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -22,41 +30,54 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private static final int CHOOSE_PIC_CODE = 12345;
+    private static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 258;
 
     Button changeWallpaper;
+    Button refresh;
     Uri picUri;
 
     /* TODO modify below method
     * getCameraImages - should get the Uri of all photos and store into an array of strings
     * but the query method makes the app crash */
+    @SuppressWarnings({"SecurityException", "MissingPermission"})
     public List<String> getCameraImages(Context context) {
 
-        /*
+
         String[] projection = {MediaStore.Images.Media.DATA};
         String selection = MediaStore.Images.Media.BUCKET_ID + " = ?";
-        String CAMERA_IMAGE_BUCKET_ID = String.valueOf(Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera".toLowerCase().hashCode());
+        String CAMERA_IMAGE_BUCKET_NAME = Environment.getExternalStorageDirectory().toString() + "/DCIM/Camera";
+
+        String CAMERA_IMAGE_BUCKET_ID = String.valueOf(CAMERA_IMAGE_BUCKET_NAME.toLowerCase().hashCode());
         String[] selectionArgs = {CAMERA_IMAGE_BUCKET_ID};
         ContentResolver contentResolver = context.getContentResolver();
+
+        // The query method is what makes this crash
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 258);
+
         //Cursor cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
         Cursor cursor = contentResolver.query(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, projection, selection, selectionArgs, null);
-        List<String> uriArray = new ArrayList<String>(cursor.getCount());
+
+        List<String> pathNames = new ArrayList<String>(cursor.getCount());
         if (cursor.moveToFirst()) {
             final int dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
             do {
                 final String data = cursor.getString(dataColumn);
-                uriArray.add(data);
+                pathNames.add(data);
             } while (cursor.moveToNext());
         }
-        cursor.close();
-        return uriArray;
-        */
 
+        cursor.close();
+        return pathNames;
+
+/*
         // test strings since above code isn't working for now
         List<String> asdf = new ArrayList<String>();
         asdf.add("test - 3");
         asdf.add("test - 2");
         asdf.add("test - 1");
         return asdf;
+        */
 
     }
 
@@ -111,15 +132,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<String> a = getCameraImages(getApplicationContext());
-        for (int i = 0; i < a.size(); i++) {
-            Toast.makeText(getApplicationContext(), a.get(i), Toast.LENGTH_SHORT).show();
+        while (!checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+            checkPermissionREAD_EXTERNAL_STORAGE(this);
         }
 
         Toast.makeText(getApplicationContext(), "App Started", Toast.LENGTH_SHORT).show();
 
-        /* change wallpaper button */
-        changeWallpaper = (Button) findViewById(R.id.changeWallpaper);
+        List<String> pathNames = getCameraImages(getApplicationContext());
+        List<Uri> uriNames = new ArrayList<>();
+
+        for (int i = 0; i < pathNames.size(); i++) {
+            Toast.makeText(getApplicationContext(), pathNames.get(i), Toast.LENGTH_SHORT).show();
+            uriNames.add(Uri.parse(pathNames.get(i)));
+        }
+
+        /* onClick listeners for buttons */
+        changeWallpaper = (Button) findViewById(R.id.chooseWallpaper);
+        refresh = (Button) findViewById(R.id.refresh);
 
         changeWallpaper.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,7 +157,61 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
+        refresh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getCameraImages(getApplicationContext());
+            }
+        });
     }
+
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] { Manifest.permission.READ_EXTERNAL_STORAGE },
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+
+
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+
 }
 
