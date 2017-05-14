945c0bfded526f89c2fabfabf7d8782004be2542
Rohit Godbole <rgodbole@ucsd.edu>
2017-05-14 00:12:46 -0700

    fixed code style a little

--- app/src/main/java/com/example/cs110sau/dejaphoto/ExampleAppWidgetProvider.java
+++ app/src/main/java/com/example/cs110sau/dejaphoto/ExampleAppWidgetProvider.java
@@ -23,34 +23,7 @@ public class ExampleAppWidgetProvider extends AppWidgetProvider {
 
         appWidgetManager.updateAppWidget(appWidgetIds, remoteViews);
     }
-/*
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
 
-        int appWidgetId = appWidgetIds[0];
-
-        // Create an Intent to launch ExampleActivity
-        Intent intent = new Intent(context, MainActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
-
-        Intent nextPhotoIntent = new Intent(context, NextPhotoActivity.class);
-        PendingIntent nextPhotoPendingIntent = PendingIntent.getActivity(context, 0, nextPhotoIntent, 0);
-
-        // Get the layout for the App Widget and attach an on-click listener
-        // to the button
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget_layout);
-        views.setOnClickPendingIntent(R.id.Right, nextPhotoPendingIntent);
-
-        views.setOnClickPendingIntent(R.id.Left, pendingIntent);
-
-        views.setOnClickPendingIntent(R.id.Karma, pendingIntent);
-
-        views.setOnClickPendingIntent(R.id.Release, pendingIntent);
-
-        // Tell the AppWidgetManager to perform an update on the current app widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
-
-    }
-*/
 }
 
 /*
