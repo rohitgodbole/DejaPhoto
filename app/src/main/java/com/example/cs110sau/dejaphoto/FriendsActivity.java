d36304aae7ff6560ea10ad5b6c14d3aeaaaabe40
larissayiyi <l4johnso@ucsd.edu>
2017-06-09 23:52:33 -0700

    DejaPhotoFriends view album

--- app/src/main/java/com/example/cs110sau/dejaphoto/FriendsActivity.java
+++ app/src/main/java/com/example/cs110sau/dejaphoto/FriendsActivity.java
@@ -1,7 +1,11 @@
 package com.example.cs110sau.dejaphoto;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -11,18 +15,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+
 public class FriendsActivity extends AppCompatActivity {
 
+    String root = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
         final SharedPreferences sharedPreferences = getSharedPreferences("user_name", MODE_PRIVATE);
         Button addFriends;
+        Button viewfriendsphoto;
         String friendID;
 
         addFriends = (Button) findViewById(R.id.addFriends);
         friendID = ((EditText) findViewById(R.id.friendid)).getEditableText().toString();
+        viewfriendsphoto = (Button) findViewById(R.id.viewFriendsPhoto);
+
         addFriends.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View view) {
@@ -33,6 +43,21 @@ public class FriendsActivity extends AppCompatActivity {
 
 
             }
+        });
+        viewfriendsphoto.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick (View view)  {
+                File myDir = new File(root, "/DejaPhotoFriends");
+                myDir.mkdirs();
+             //   intent.setAction(Intent.ACTION_VIEW);
+             //   intent.setDataAndType(Uri.parse(myDir, "image/*");
+              //  startActivity(intent);
+                Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                photoPickerIntent.setType("image/*");
+                photoPickerIntent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                startActivity(photoPickerIntent);
+            }
+
         });
 
     }
