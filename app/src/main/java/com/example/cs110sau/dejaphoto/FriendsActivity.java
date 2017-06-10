76082180da3692fba0b94a37867a3c0ddbcf0fa7
larissayiyi <l4johnso@ucsd.edu>
2017-06-09 21:30:17 -0700

    friends activity add friends

--- app/src/main/java/com/example/cs110sau/dejaphoto/FriendsActivity.java
+++ app/src/main/java/com/example/cs110sau/dejaphoto/FriendsActivity.java
@@ -1,11 +1,15 @@
 package com.example.cs110sau.dejaphoto;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FriendsActivity extends AppCompatActivity {
 
@@ -13,19 +17,24 @@ public class FriendsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        final SharedPreferences sharedPreferences = getSharedPreferences("user_name", MODE_PRIVATE);
+        Button addFriends;
+        String friendID;
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        addFriends = (Button) findViewById(R.id.addFriends);
+        friendID = ((EditText) findViewById(R.id.friendid)).getEditableText().toString();
+        addFriends.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick (View view) {
+                String userid = sharedPreferences.getString("userid", null);
+                if (userid.equals("")){
+                    Toast.makeText(getApplicationContext(), "You must create an user ID first.", Toast.LENGTH_LONG).show();
+                }
+
             }
         });
-    }
 
+    }
     public void finish (View view) {
         finish();
     }
